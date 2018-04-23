042bbbc9cc97c456acf3956285915156d1c55c15
Mehdi Khanmohammadi <mehdiokhan@gmail.com>
2018-04-23 14:01:27 +0430

    Static root added to settings

--- languagetheory/settings.py
+++ languagetheory/settings.py
@@ -112,6 +112,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT= os.path.join(PROJECT_DIR,'static/')
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
