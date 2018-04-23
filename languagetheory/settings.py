63b2ab1e83d6a0f2431c19e90e811ac000dec8b2
Mahdi Khanmohammadi <Mehdiokhan@gmail.com>
2018-04-23 16:12:18 +0430

    Update settings.py

--- languagetheory/settings.py
+++ languagetheory/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'v^2n)=r3yvcx!9wdp(ocw^g84+uo(&z&5%(&%lfny_)2)2p3)0'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
