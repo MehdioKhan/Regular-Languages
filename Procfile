9955661fee5e0dd3e0f5b14d33368ce462f727a8
Mehdi Khanmohammadi <mehdiokhan@gmail.com>
2018-04-23 13:11:19 +0430

    aditional files and changes for Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn languagetheory.wsgi --log-file -
--- languagetheory/wsgi.py
+++ languagetheory/wsgi.py
@@ -14,3 +14,9 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "languagetheory.settings")
 
 application = get_wsgi_application()
+
+
+# Added for Production
+
+from whitenoise.django import DjangoWhiteNoise
+application = DjangoWhiteNoise(application)
