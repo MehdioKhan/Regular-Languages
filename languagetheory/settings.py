60a7a43c63cd999715a2da4494f0e04dbddec80c
Mehdi Khanmohammadi <mehdiokhan@gmail.com>
2018-04-23 15:52:46 +0430

    Project dir deleted

--- languagetheory/settings.py
+++ languagetheory/settings.py
@@ -3,7 +3,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-PROJECT_DIR=os.path.dirname(__file__)
+# PROJECT_DIR=os.path.dirname(__file__)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
@@ -112,6 +112,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT= os.path.join(PROJECT_DIR,'static/')
+STATIC_ROOT= os.path.join(BASE_DIR,'static')
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
